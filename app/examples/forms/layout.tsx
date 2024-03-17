bc56c4b40819730516d9aef5d4c32d50f2bb7073
GHGHGHKO <gudrb963@gmail.com>
2024-03-17 15:51:12 +0900

    feat: delete unused page on `forms`

--- app/examples/forms/layout.tsx
+++ app/examples/forms/layout.tsx
@@ -14,22 +14,6 @@ const sidebarNavItems = [
     title: "Profile",
     href: "/examples/forms",
   },
-  {
-    title: "Account",
-    href: "/examples/forms/account",
-  },
-  {
-    title: "Appearance",
-    href: "/examples/forms/appearance",
-  },
-  {
-    title: "Notifications",
-    href: "/examples/forms/notifications",
-  },
-  {
-    title: "Display",
-    href: "/examples/forms/display",
-  },
 ]
 
 interface SettingsLayoutProps {
