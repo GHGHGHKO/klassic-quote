06de44f52273d927b282f46508169fc0fab22062
GHGHGHKO <gudrb963@gmail.com>
2024-03-17 21:39:22 +0900

    feat: modify forms description

--- app/examples/forms/page.tsx
+++ app/examples/forms/page.tsx
@@ -6,7 +6,7 @@ import {ProfileForm} from "@/app/examples/forms/profile-form";
 
 const sidebarNavItems = [
     {
-        title: "Profile",
+        title: "타짜 명대사",
         href: "/examples/forms",
     },
 ]
@@ -33,9 +33,9 @@ export default function FormsPage() {
             </div>
             <div className="hidden space-y-6 p-10 pb-16 md:block">
                 <div className="space-y-0.5">
-                    <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+                    <h2 className="text-2xl font-bold tracking-tight">명대사 API 사용하기</h2>
                     <p className="text-muted-foreground">
-                        Manage your account settings and set e-mail preferences.
+                        버튼을 눌러 API를 호출하면 Response body를 확인 할 수 있습니다.
                     </p>
                 </div>
                 <Separator className="my-6" />
@@ -45,9 +45,9 @@ export default function FormsPage() {
                     </aside>
                     <div className="space-y-6">
                         <div>
-                            <h3 className="text-lg font-medium">Profile</h3>
+                            <h3 className="text-lg font-medium">타짜</h3>
                             <p className="text-sm text-muted-foreground">
-                                This is how others will see you on the site.
+                                타짜 명대사를 볼 수 있는 API 입니다.
                             </p>
                         </div>
                         <Separator />
