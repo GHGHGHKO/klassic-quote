625e1cfcfae4c77c852af859ea223b1dab649bdd
GHGHGHKO <gudrb963@gmail.com>
2024-03-17 15:52:50 +0900

    feat: delete unused page on nav

--- components/examples-nav.tsx
+++ components/examples-nav.tsx
@@ -14,39 +14,9 @@ const examples = [
     code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/mail",
   },
   {
-    name: "Dashboard",
-    href: "/examples/dashboard",
-    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/dashboard",
-  },
-  {
-    name: "Cards",
-    href: "/examples/cards",
-    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/cards",
-  },
-  {
-    name: "Tasks",
-    href: "/examples/tasks",
-    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/tasks",
-  },
-  {
-    name: "Playground",
-    href: "/examples/playground",
-    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/playground",
-  },
-  {
     name: "Forms",
     href: "/examples/forms",
     code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/forms",
-  },
-  {
-    name: "Music",
-    href: "/examples/music",
-    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/music",
-  },
-  {
-    name: "Authentication",
-    href: "/examples/authentication",
-    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/authentication",
   },
 ]
 
