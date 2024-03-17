731b0062ab48d4e31870f81203fa8d47362a4d9f
GHGHGHKO <gudrb963@gmail.com>
2024-03-17 16:51:47 +0900

    feat: add separator on default page

--- app/examples/forms/default-page.tsx
+++ app/examples/forms/default-page.tsx
@@ -43,7 +43,16 @@ export default function FormsPage() {
                     <aside className="-mx-4 lg:w-1/5">
                         <SidebarNav items={sidebarNavItems} />
                     </aside>
-                    <ProfileForm />
+                    <div className="space-y-6">
+                        <div>
+                            <h3 className="text-lg font-medium">Profile</h3>
+                            <p className="text-sm text-muted-foreground">
+                                This is how others will see you on the site.
+                            </p>
+                        </div>
+                        <Separator />
+                        <ProfileForm />
+                    </div>
                 </div>
             </div>
         </>
