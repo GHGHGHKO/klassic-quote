8307b7da018daeab0e8199ef35a8fe44ad5ad3e3
GHGHGHKO <gudrb963@gmail.com>
2024-05-13 22:38:12 +0900

    feat: add API copy icon

--- app/examples/forms/account-form.tsx
+++ app/examples/forms/account-form.tsx
@@ -1,22 +1,17 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
 
-import { Button } from "@/registry/new-york/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/registry/new-york/ui/form"
-import { Input } from "@/registry/new-york/ui/input"
-import { toast } from "@/registry/new-york/ui/use-toast"
+import {Button} from "@/registry/new-york/ui/button"
+import {Form, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/registry/new-york/ui/form"
+import {Input} from "@/registry/new-york/ui/input"
+import {toast} from "@/registry/new-york/ui/use-toast"
 import {Quote} from "@/data/quote";
+import {Label} from "@/registry/new-york/ui/label";
+import {CheckIcon, CopyIcon} from "@radix-ui/react-icons";
+import {useState} from "react";
 
 const accountFormSchema = z.object({
   URL: z.string()
@@ -25,6 +20,8 @@ const accountFormSchema = z.object({
 type AccountFormValues = z.infer<typeof accountFormSchema>
 
 export function AccountForm({ randomQuoteLink }: { randomQuoteLink: string }) {
+    const [clicked, setClicked] = useState(false);
+
     const defaultValues: Partial<AccountFormValues> = {
         URL: randomQuoteLink,
     }
@@ -56,6 +53,17 @@ export function AccountForm({ randomQuoteLink }: { randomQuoteLink: string }) {
           });
   }
 
+    const handleCopy = (textToCopy: string) => {
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                setClicked(true);
+                console.log('Text copied to clipboard');
+            })
+            .catch(err => {
+                console.error('Unable to copy text to clipboard:', err);
+            });
+    };
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(fetchRandomQuote)} className="space-y-8">
@@ -65,9 +73,23 @@ export function AccountForm({ randomQuoteLink }: { randomQuoteLink: string }) {
           render={({ field }) => (
             <FormItem>
               <FormLabel>API URL</FormLabel>
-              <FormControl>
-                <Input placeholder="Your name" {...field} readOnly={true} />
-              </FormControl>
+                <div className="flex items-center space-x-2 pt-4">
+                    <div className="grid flex-1 gap-2">
+                        <Label htmlFor="link" className="sr-only">
+                            Link
+                        </Label>
+                        <Input
+                            id="link"
+                            {...field}
+                            readOnly
+                            className="h-9"
+                        />
+                    </div>
+                    <Button type="submit" size="sm" className="px-3" onClick={() => handleCopy(field.value)}>
+                        <span className="sr-only">Copy</span>
+                        {clicked ? <CheckIcon className="h-4 w-4" /> : <CopyIcon className="h-4 w-4" />}
+                    </Button>
+                </div>
               <FormDescription>
                   GET Method로 API를 호출합니다.
                   열려 있으니 언제든지 호출해도 됩니다! PR도 환영해요~
